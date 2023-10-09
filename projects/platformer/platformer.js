5f83924aceb285dafbafaa3a1220682c9a746465
SethM M <agentseth13@gmail.com>
2023-10-09 16:35:53 +0000

    added platforms and collectables to game

--- projects/platformer/platformer.js
+++ projects/platformer/platformer.js
@@ -28,12 +28,12 @@ $(function () {
      * Comment the lines out to remove the grid
      */
 
-    // for (let i = 100; i < canvas.width; i += 100) {
-    //   createPlatform(i, canvas.height, -1, -canvas.height);
-    // }
-    // for (let i = 100; i < canvas.height; i += 100) {
-    //   createPlatform(canvas.width, i, -canvas.width, -1);
-    // }
+    for (let i = 100; i < canvas.width; i += 100) {
+      createPlatform(i, canvas.height, -1, -canvas.height);
+    }
+    for (let i = 100; i < canvas.height; i += 100) {
+      createPlatform(canvas.width, i, -canvas.width, -1);
+    }
 
     /////////////////////////////////////////////////
     //////////ONLY CHANGE BELOW THIS POINT///////////
@@ -44,7 +44,14 @@ $(function () {
     // You must decide the x position, y position, width, and height of the platforms
     // example usage: createPlatform(x,y,width,height)
 
-
+    createPlatform(1200, 600, 115, 1)
+    createPlatform(1257, 460, 87, 1)
+    createPlatform(1305, 460, 10, 1)
+    createPlatform(1200, 600, 10, 1)
+    createPlatform(1005, 500, 100, 1)
+    createPlatform(650, 410, 100, 1)
+    createPlatform(810, 519, 100, 1)
+    createPlatform(400, 519, 100, 1)
 
     
     // TODO 2
@@ -54,7 +61,9 @@ $(function () {
     // example usage: createCollectable(type, x, y, gravity, bounce)
 
 
-
+    createCollectable("diamond", 807, 415, .5)
+    createCollectable("diamond", 1250, 460, .5)
+    createCollectable("diamond", 1345, 600, .5)
 
     // TODO 3
     // Create cannons
@@ -62,7 +71,9 @@ $(function () {
     // Your wall choices are: 'top' 'left' 'right' and 'bottom'
     // example usage: createCannon(side, position, delay, width, height)
 
-
+    createCannon("top", 1000, 1000)
+    createCannon("right", 415, 1, 1, 1, 10)
+    createCannon("right", 600, 2000)
 
 
     /////////////////////////////////////////////////
